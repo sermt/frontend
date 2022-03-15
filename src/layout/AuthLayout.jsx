1dae32867631aec0e59774ae9ec642b30526bd7c
sermt <sermi.cima@gmail.com>
2022-03-15 03:16:15 -0600

    first1 commits3

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <>
-      
       <main className="container mx-auto md:grid grid-cols-2 mt-5 p-5 gap-10">
       <Outlet />
       </main>
