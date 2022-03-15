fd9aaf017c1a956e0a534cdeb29fe76639b28004
sermt <sermi.cima@gmail.com>
2022-03-15 01:15:11 -0600

    first1 commits

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet, Navigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import useAuth from "../hooks/useAuth";
-
 export default function AdminLayout() {
   const { auth, loading } = useAuth();
   if (loading) return "Loading...";
