69f06af3387003b6b43b8bced31d88dd8c97d406
sermt <sermi.cima@gmail.com>
2022-03-16 15:54:44 -0600

    first1 commits31111

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="md:fixed block bottom-0 w-screen py-10 bg-slate-400">
-        <p className="text-center font-bold text-1xl"> React Project- <span className="text-white">Veterinaries</span></p>
+    <footer className="md:fixed block bottom-0 w-screen py-10 bg-indigo-500 text-center">
+        <p className=" font-bold text-1xl"> React Project- <span className="text-indigo-700">Veterinaries</span></p>
     </footer>
   )
 }
