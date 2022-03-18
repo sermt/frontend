e2f31cd2609506d2b7213c3affab3f0c27f5c400
sermt <sermi.cima@gmail.com>
2022-03-17 20:37:53 -0600

    first1 commits31111

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,25 +5,34 @@ import { bindActionCreators } from "redux";
 import { actionCreators } from "../redux";
 export default function Header() {
   const dispatch = useDispatch();
-  const {logout} =bindActionCreators(actionCreators, dispatch)
+  const { logout } = bindActionCreators(actionCreators, dispatch);
 
   return (
-    <header className="py-10 bg-indigo-400">
+    <header className="py-10 bg-indigo-600">
       <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
-      <Link to="/admin" className="hover:bg-indigo-700 bg-text-white text-sm font-black hover:cursor-pointer uppercase ">
-            Patients
-          </Link>
+        <Link
+          to="/admin"
+          className="hover:bg-indigo-850 bg-text-white text-sm font-bold hover:cursor-pointer uppercase "
+        >
+          Vet App
+        </Link>
 
         <nav className="flex flex-col lg:flex-row items-center justify-between gap-4 mt-5 lg:mt-0">
-          <Link to="/admin" className="hover:bg-indigo-700 bg-text-white text-sm font-black hover:cursor-pointer uppercase ">
+          <Link
+            to="/admin"
+            className="hover:bg-indigo-850 bg-text-white text-sm font-black hover:cursor-pointer uppercase "
+          >
             Patients
           </Link>
-          <Link to="/admin/profile" className="hover:bg-indigo-700 text-white text-sm font-black hover:cursor-pointer uppercase">
+          <Link
+            to="/admin/profile"
+            className="hover:bg-indigo-850 text-white text-sm font-black hover:cursor-pointer uppercase"
+          >
             Profile
           </Link>
           <button
             type="button"
-            className="hover:bg-indigo-700 text-white  text-sm font-black uppercase hover:cursor-pointer"
+            className="hover:bg-indigo-850 text-white  text-sm font-black uppercase hover:cursor-pointer"
             onClick={logout}
           >
             Log out
