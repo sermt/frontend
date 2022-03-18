399953052917cab9913573c792dba5d066cfb32d
sermt <sermi.cima@gmail.com>
2022-03-17 22:12:26 -0600

    first1 commits31111

--- src/components/AdminNav.jsx
+++ src/components/AdminNav.jsx
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom'
 export default function AdminNav() {
   return (
     <nav className="flex gap-3">
-            <Link to="/admin/profile" className="font-bold uppercase text-gray-500">Profile</Link>
-            <Link to="/admin/change-password" className="font-bold uppercase text-gray-500">Change password</Link>
+            <Link to="/admin/profile" className="hover:text-gray-500 hover:cursor-pointer font-bold uppercase text-gray-800">Edit profile</Link>
+            <Link to="/admin/change-password" className="hover:text-gray-500 hover:cursor-pointer font-bold uppercase text-gray-800">Change password</Link>
     </nav>
   )
 }
